76ed304a0c7691b21093e2377fdf6c340d28376d
Edson <edsaraujocorral12@gmail.com>
2020-06-07 18:04:16 -0300

    Validacoes de Agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,6 @@
 import Appointment from '../models/Appointments';
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 
 class AppointmentController {
@@ -9,7 +10,7 @@ class AppointmentController {
             data: Yup.date().required(),
         });
         if (!(await schema.isValid(req.body))) {
-            return res.state(400).json({ error: 'Validation fails'});
+            return res.state(400).json({ error: 'Validation fails' });
         }
 
         const { provider_id, data } = req.body;
@@ -18,17 +19,38 @@ class AppointmentController {
          * Check if provider_id is a provider
          */
         const isProvider = await User.findOne({
-            where: { id: provider_id, provider: true}
+            where: { id: provider_id, provider: true }
         });
 
         if (!isProvider) {
-            return res.status(401).json({error: 'You can only create appointments with providers'});
+            return res.status(401).json({ error: 'You can only create appointments with providers' });
         }
+        /**
+         * Check for past date
+         */
+        const hourStart = startOfHour(parseISO(data));
 
+        if (isBefore(hourStart, new Date())) {
+            return res.status(400).json({ error: 'Past dates are not permitted' });
+        }
+        /**
+         * Check data availability
+         */
+        const checkAvailability = await Appointment.findOne({
+            where: {
+                provider_id,
+                canceled_at: null,
+                data: hourStart
+            },
+        });
+
+        if (checkAvailability) {
+            return res.status(400).json({ error: 'Appointment date is not avaible' });
+        }
         const appointment = await Appointment.create({
             user_id: req.userId,
             provider_id,
-            data,
+            data: hourStart
         });
 
         return res.json(appointment);
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -10,20 +10,20 @@ class User extends Model {
             password_hash: Sequelize.STRING,
             provider: Sequelize.BOOLEAN
         },
-        {
-           sequelize,
-        }
-    );
-    this.addHook("beforeSave", async (user) => {
-        if (user.password) {
-            user.password_hash = await bcrypt.hash(user.password, 8);
-        }
-    });
-    return this;
+            {
+                sequelize,
+            }
+        );
+        this.addHook("beforeSave", async (user) => {
+            if (user.password) {
+                user.password_hash = await bcrypt.hash(user.password, 8);
+            }
+        });
+        return this;
     }
 
     static associate(models) {
-        this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar'});
+        this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
     }
     checkPassword(password) {
         return bcrypt.compare(password, this.password_hash);
