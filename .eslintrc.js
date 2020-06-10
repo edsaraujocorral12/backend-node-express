8f237791f09886f12b6324baa9712a55189f7044
Edson <edsaraujocorral12@gmail.com>
2020-06-10 09:09:39 -0300

    Monitorando falha na fila

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: [
-    'airbnb-base', 'prettier'
-  ],
-  plugins: ['prettier'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 11,
-    sourceType: 'module',
-  },
-  rules: {
-    "prettier/prettier":"error",
-    "class-methods-use-this": "off",
-    "no-param-ressign": "off",
-    "camelcase": "off",
-    "no-unused-vars": ["error", { "argsignorePattern": "next"}]
-  },
+    env: {
+        node: true,
+        es6: true,
+    },
+    extends: [
+        'airbnb-base', 'prettier'
+    ],
+    plugins: ['prettier'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 11,
+        sourceType: 'module',
+    },
+    rules: {
+        "prettier/prettier": "error",
+        "class-methods-use-this": "off",
+        "no-param-ressign": "off",
+        "camelcase": "off",
+        "no-unused-vars": ["error", { "argsignorePattern": "next" }]
+    },
 };
--- src/app.js
+++ src/app.js
@@ -4,21 +4,21 @@ import './database';
 import path from 'path';
 
 class App {
-  constructor() {
-    this.server = express();
+    constructor() {
+        this.server = express();
 
-    this.middlewares();
-    this.routes();
-  }
+        this.middlewares();
+        this.routes();
+    }
 
-  middlewares() {
-    this.server.use(express.json());
-    this.server.use('/files', express.static(path.resolve(__dirname, '..','temp', 'uploads')));
-  }
+    middlewares() {
+        this.server.use(express.json());
+        this.server.use('/files', express.static(path.resolve(__dirname, '..', 'temp', 'uploads')));
+    }
 
-  routes() {
-    this.server.use(routes);
-  }
+    routes() {
+        this.server.use(routes);
+    }
 }
 
 export default new App().server;
